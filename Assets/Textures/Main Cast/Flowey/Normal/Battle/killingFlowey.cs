f8a566e4db0a83fa07972b9b001095164dd34995
Kevin Bell <48041104+StayPure@users.noreply.github.com>
2020-01-31 18:12:37 -0500

    Add files via upload

---
+++ Assets/Textures/Main Cast/Flowey/Normal/Battle/killingFlowey.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killingFlowey : MonoBehaviour
+{
+    public GameObject kF;
+
+    void startHit()
+    {
+        GetComponent<Animator>().SetBool("hit", true);
+        
+    }
+
+    void stopHit()
+    {
+        GetComponent<Animator>().SetBool("hit", false);
+        kF.GetComponent<Animator>().SetInteger("numOfHits", kF.GetComponent<Animator>().GetInteger("numOfHits") + 1);
+
+        if(kF.GetComponent<Animator>().GetInteger("numOfHits") == 8)
+        {
+            GetComponent<Animator>().SetBool("done", true);
+        }
+    }
+}
