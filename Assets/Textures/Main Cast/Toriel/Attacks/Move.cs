a7d2b181a4b932d4b1d4b3125d3eafb08e3972ba
Kevin Bell <48041104+StayPure@users.noreply.github.com>
2020-02-01 03:48:16 -0500

    Add files via upload

---
+++ Assets/Textures/Main Cast/Toriel/Attacks/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector2 velocity;  
+    public float angularVelocity;  
+    public float duration;
+
+    public void execute (float stepSize, Transform avatar) {
+        avatar.Translate(avatar.forward * velocity * stepSize);
+    }
+}
---
+++ Assets/Textures/Main Cast/Toriel/Attacks/MoveSet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSet : MonoBehaviour
+{
+    public Vector2 spawnOffset; //optional, but if you need this, best put it here  
+    public Move[] moves;  
+    private int currentMove = 0;
+    private float moveTime;  
+
+bool execute(float stepSize, Transform avatar) {  
+  moves[currentMove].execute(stepSize, avatar);  
+  moveTime += stepSize;  
+  if (moveTime >= moves[currentMove].duration) {  
+    currentMove += 1;  
+    moveTime = 0;  
+    if (currentMove >= moves.Length)  
+      return true; //move set finished - repeat set or despawn enemy etc.  
+  }  
+  return false;  
+}   
+}
